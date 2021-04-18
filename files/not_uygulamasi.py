9e2948904dc51c5764849ebaf0c2e8ac1df582c3
Mehmet Alper Derin <malperderin@outlook.com>
2021-04-18 18:24:01 +0300

    updates

--- files/not_uygulamasi.py
+++ files/not_uygulamasi.py
@@ -3,15 +3,24 @@ def not_hesapla(satir):
     liste = satir.split(':')
     
     ogrenciAdi = liste[0]
-    notlar = liste[1]
+    notlar = liste[1].split(',')
 
-    not1 = notlar[0]
-    not2 = notlar[1]
-    not3 = notlar[2]
+    not1 = int(notlar[0])
+    not2 = int(notlar[1])
+    not3 = int(notlar[2])
 
     ortalama = (not1+not2+not3)/3
 
-    
+    if ortalama >= 90 and ortalama <= 100:
+        harf = "AA"
+    elif ortalama >= 85 and ortalama <= 89:
+        harf = "BA"
+    elif ortalama >= 65:
+        harf = "CC"
+    else:
+        harf = "FF"
+
+    return ogrenciAdi +  ":" + harf + "\n"   
 
 def ortalamari_oku():
     with open('sinav_notlari.txt','r', encoding='utf-8') as file:
@@ -30,7 +39,15 @@ def not_gir():
         file.write(ad+' '+ soyad+ ':'+ not1+ ','+ not2 + ',' + not3+ '\n')
 
 def notlari_kaydet():
-    pass
+    with open("sinav_notlari.txt","r", encoding="utf-8") as file:
+        liste = []
+
+        for i in file:
+            liste.append(not_hesapla(i))
+
+        with open("sonuclar.txt","w", encoding="utf-8") as file2:
+            for i in liste:
+                file2.write(i)
 
 while True:
     islem = input('1- Not Oku\n2- Not Gir\n3- Notları Kayıt Et\n4- Çıkış\n')
