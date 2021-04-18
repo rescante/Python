67b2e85939a9bf2746aff5ec789793e6fb8a20dc
Mehmet Alper Derin <malperderin@outlook.com>
2021-04-18 18:30:45 +0300

    updates

--- files/not_uygulamasi.py
+++ files/not_uygulamasi.py
@@ -15,8 +15,18 @@ def not_hesapla(satir):
         harf = "AA"
     elif ortalama >= 85 and ortalama <= 89:
         harf = "BA"
-    elif ortalama >= 65:
+    elif ortalama >= 80 and ortalama <= 84:
+        harf = "BB"
+    elif ortalama >= 75 and ortalama <= 79:
+        harf = "CB"
+    elif ortalama >= 70 and ortalama <= 74:
         harf = "CC"
+    elif ortalama >= 65 and ortalama <= 69:
+        harf = "DC"
+    elif ortalama >= 60 and ortalama <= 64:
+        harf = "DD"
+    elif ortalama >= 50 and ortalama <= 59:
+        harf = "FD"
     else:
         harf = "FF"
 
